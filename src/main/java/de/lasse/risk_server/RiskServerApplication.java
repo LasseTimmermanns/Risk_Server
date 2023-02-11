a4c07788690b8aae6bff3d7220970916a1047917
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-02-11 14:40:31 +0100

    initial commit

---
+++ src/main/java/de/lasse/risk_server/RiskServerApplication.java
@@ -0,0 +1,13 @@
+package de.lasse.risk_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lasse/risk_server/RiskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lasse.risk_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
