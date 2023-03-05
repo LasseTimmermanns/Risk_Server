2afbc920e5dcbe00da36c7c97f7b41a6894fb08a
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-03-05 13:55:01 +0100

    Script to create static JSON-File from Map rscs; API-Endpoint to request Map-Information

--- src/main/java/de/lasse/risk_server/Controller/MapController.java
+++ src/main/java/de/lasse/risk_server/Controller/MapController.java
@@ -1,6 +1,9 @@
 package de.lasse.risk_server.Controller;
 
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,17 +12,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.lasse.risk_server.MapProcessing.MapProcessing;
+import de.lasse.risk_server.Utils.FileUtils;
 
 @RestController
 @CrossOrigin
 public class MapController {
 
-    @RequestMapping(value = "/maps/{map_name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/maps/{map_name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<String> getMap(@PathVariable String map_name) {
-        String body = MapProcessing.createMapObject(map_name).toJson().toString();
-        return new ResponseEntity<String>(body, HttpStatus.ACCEPTED);
+        try {
+            String body = FileUtils.getFileAsString("static/maps/jsons/" + map_name + ".json");
+            return new ResponseEntity<String>(body, HttpStatus.ACCEPTED);
+        } catch (IOException e) {
+            System.out.println("Couldnt find " + map_name);
+            return new ResponseEntity<String>("Map not found", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
