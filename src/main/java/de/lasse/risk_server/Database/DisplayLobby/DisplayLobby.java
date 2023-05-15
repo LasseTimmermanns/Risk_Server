8e531ee276b952b9d10983665835f9fc867d0f7e
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-05-15 10:56:48 +0200

    Extended DisplayLobby class with turnTimer, cardBonus

--- src/main/java/de/lasse/risk_server/Database/DisplayLobby/DisplayLobby.java
+++ src/main/java/de/lasse/risk_server/Database/DisplayLobby/DisplayLobby.java
@@ -14,12 +14,18 @@ public class DisplayLobby {
 
     private String host;
 
+    private int turnTimer;
+
+    private String cardBonus;
+
     private String mapId;
 
     public JSONObject toJsonObject() {
         JSONObject out = new JSONObject();
         out.put("id", id);
         out.put("mapId", mapId);
+        out.put("turnTimer", turnTimer);
+        out.put("cardBonus", cardBonus);
         out.put("playerCount", playerCount);
         out.put("maxPlayers", maxPlayers);
         out.put("host", host);
