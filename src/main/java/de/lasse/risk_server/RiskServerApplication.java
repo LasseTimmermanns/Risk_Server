dcec546b48ff59f3111da0d902e46257cd761d5c
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-05-16 13:44:20 +0200

    Lobby Collection now cleared after Startup

--- src/main/java/de/lasse/risk_server/RiskServerApplication.java
+++ src/main/java/de/lasse/risk_server/RiskServerApplication.java
@@ -1,13 +1,26 @@
 package de.lasse.risk_server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import de.lasse.risk_server.Database.Lobby.LobbyInterfaceRepository;
 
 @SpringBootApplication
 public class RiskServerApplication {
 
+	@Autowired
+	LobbyInterfaceRepository lobbyInterfaceRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RiskServerApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void clearLobbyInterface() {
+		lobbyInterfaceRepository.deleteAll();
 	}
 
 }
