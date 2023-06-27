76ad6f83dc72fe871d24af5c5e4db6f03bd73cef
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-06-27 15:02:48 +0200

    Changed DataRetrieving to Interface Based

--- src/main/java/de/lasse/risk_server/Lobby/Settings/SettingsService.java
+++ src/main/java/de/lasse/risk_server/Lobby/Settings/SettingsService.java
@@ -2,18 +2,26 @@ package de.lasse.risk_server.Lobby.Settings;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import de.lasse.risk_server.Game.Map.MapTerritory;
+import de.lasse.risk_server.Game.Players.Player;
+import de.lasse.risk_server.Game.Settings.SettingsState;
 import de.lasse.risk_server.Lobby.LobbyHandler;
 import de.lasse.risk_server.Lobby.Lobby.Lobby;
 import de.lasse.risk_server.Lobby.Lobby.LobbyInterfaceRepository;
 import de.lasse.risk_server.Lobby.LobbyPlayer.LobbyPlayer;
 import de.lasse.risk_server.Shared.QueryIdentification;
 import de.lasse.risk_server.Shared.WebSocketHelper;
+import de.lasse.risk_server.Shared.Color.Color;
 
 @Service
 public class SettingsService {
@@ -25,7 +33,7 @@ public class SettingsService {
     PlayerSettingsService playerSettingsService;
 
     public boolean isAuthorized(Lobby lobby, String token) {
-        Optional<LobbyPlayer> player = Arrays.stream(lobby.getPlayers())
+        Optional<LobbyPlayer> player = Arrays.stream(lobby.getLobbyPlayers())
                 .filter(p -> p.getHost() && p.getToken().equals(token))
                 .findFirst();
         return player.isPresent();
@@ -115,7 +123,54 @@ public class SettingsService {
         if (lobby == null)
             return;
 
-        // Game game = new Game();
+        // Create New Game
 
+        // id;
+        // mapId;
+        // players;
+        // territories;
+        // move;
+        // settings;
+
+        String gameId = lobby.getId();
+        String mapId = lobby.getMapId();
+        int move = 0;
+        SettingsState settingsState = new SettingsState(lobby.isFixed());
+        Player[] players = getPlayers(lobby.getLobbyPlayers());
+
+        // Territories
+
+        // Initialize GameHandler
+
+        // Inform Players
+    }
+
+    private List<Integer> generateRandomOrder(int range) {
+        List<Integer> order = IntStream.range(0, range).boxed().collect(Collectors.toList());
+        Collections.shuffle(order);
+        return order;
+    }
+
+    private Player[] getPlayers(LobbyPlayer[] lobbyPlayers) {
+        Player[] out = new Player[lobbyPlayers.length];
+        List<Integer> order = generateRandomOrder(lobbyPlayers.length);
+        for (int i = 0; i < lobbyPlayers.length; i++) {
+            LobbyPlayer p = lobbyPlayers[order.get(i)];
+
+            String id = p.getId();
+            String token = p.getToken();
+            String name = p.getName();
+            Color color = p.getColor();
+            int seat = order.get(i);
+
+            out[i] = new Player(id, token, name, color, seat);
+        }
+
+        return out;
+    }
+
+    public MapTerritory[] generateTerritories(Player[] players, String mapId) {
+        // MapTerritory[] map = mapRepository.findTerritories(mapId);
+        return null;
     }
 }
